ae6303a21b90ac0bd210594de2987dc966c274d7
Breno Pereira <brenopereirabetti@gmail.com>
2024-04-29 14:45:26 -0300

    criando card de tarefas e estilizando

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,10 +1,48 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar typescript',
+    descricao: 'Ver a aula 3 da Ebac',
+    prioridade: enums.Prioridade.NORMAL,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Ir a academia',
+    descricao: 'Malhar peito',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Aniverśario',
+    descricao: 'Aniverśario do Matheus',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => {
   return (
-    <main>
+    <Container>
       <p>
         2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
       </p>
-    </main>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              titulo={t.titulo}
+              prioridade={t.prioridade}
+              status={t.status}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
   )
 }
 
